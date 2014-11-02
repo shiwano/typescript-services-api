7d9a190e6e61a2ba4ff546217c6de5fcce5536c0
Shogo Iwano <shiwano@gmail.com>
2014-11-02 14:11:41 +0900

    Fix definitions of typescript-services-api module

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,12 @@
 /// <reference path="./typescriptServices.d.ts" />
 
+// Avoid circular definition
+declare module __TypeScriptServicesApi {
+  export import __ts = ts;
+  export import __TypeScript = TypeScript;
+}
+
 declare module 'typescript-services-api' {
-  var typeScriptServicesApi: { ts: typeof ts; TypeScript: typeof TypeScript };
-  export = typeScriptServicesApi;
+  export import ts = __TypeScriptServicesApi.__ts;
+  export import TypeScript = __TypeScriptServicesApi.__TypeScript;
 }
