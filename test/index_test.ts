53872e6a017bed557f87c84f864212acb74f96bb
Shogo Iwano <shiwano@gmail.com>
2014-11-02 10:41:03 +0900

    Tweak test callbacks

--- test/index_test.ts
+++ test/index_test.ts
@@ -6,11 +6,11 @@ require('./test_helper');
 import typeScriptServices = require('typescript-services-api');
 
 describe('typescript-services-api', () => {
-  it('should export the "ts" module', function() {
+  it('should export the "ts" module', () => {
     assert(typeScriptServices.ts);
   });
 
-  it('should export the "TypeScript" module', function() {
+  it('should export the "TypeScript" module', () => {
     assert(typeScriptServices.TypeScript);
   });
 });
